65ca99bbf36b5bf56c72920540a48e59d65c6931
Emily J <ejordan04@qub.ac.uk>
2021-04-15 20:17:14 +0100

    Update Board.java
    updated version

--- Board.java
+++ Board.java
@@ -5,8 +5,14 @@ import java.util.ArrayList;
 public class Board {
 
 	public static void main(String[] args) {
+		systemBuild();
 		
 		
+		
+	}
+	
+	public static void systemBuild() {
+
 		Element e1 = new Element ("Crew Module", "Environmental Control and Life Support System", "Heat Shield","Parachutes", "Assemble Minor Developments",1);
 		Element e2 = new Element ("Service Module", "Solar Array Wings", "Fairing Panels","Spacecraft adaptor jettison fairings", "Assemble Minor Developments", 2);
 		Element e3 = new Element ("Launch Abort Module", "Jettison Motor", "Altitude Control","Abort motor", "Assemble Minor Developments", 3);
@@ -23,8 +29,6 @@ public class Board {
 		Systems orionSpacecraft = new Systems("orionSpacecraft", null, 100,system1);
 		
 		System.out.println(orionSpacecraft.toString());
-		
-		
 	}
 
 }
